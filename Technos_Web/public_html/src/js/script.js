f73488389bd6152be1c058013cdb7d447c10947f
Julie Albaladejo <julie.albaladejo@gameo-consulting.com>
2014-12-23 17:10:01 +0100

    Doublons + sort
    Suppression des départements doublons, et ordre croissant des numéros
    des départements

--- Technos_Web/public_html/src/js/script.js
+++ Technos_Web/public_html/src/js/script.js
@@ -13,8 +13,49 @@ function initMap() {
     L.marker([48.85776, 2.33939]).addTo(map)
             .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
 }
-
+function middlepop(Tab,a){
+    return (a>Tab.length)?false:(Tab.slice(0,a).concat(Tab.slice(a+1,Tab.length)));
+}
+Array.prototype.clearDoublon=function() {
+    ArrayLength=this.length;
+    var TempArray = new Array();
+    TempArray=this;
+    TempArray2=this
+    var cleared=false
+     
+    for (i=0;i<ArrayLength;i++){ 
+        for (j=0;j<TempArray.length;j++)
+            {   
+                if(!cleared &&(i==j))
+                {
+                    j++;
+                }
+            cleared=false;
+            if(TempArray[i]==TempArray2[j]){
+            TempArray2=middlepop(TempArray,j);
+            TempArray=TempArray2;
+            cleared=true;
+            j--;
+        }
+ 
+    }
+}
+return TempArray;
+}
 $(document).ready(function() {
+    var items = [];
+    $.getJSON("../json/sncf-gares-et-arrets-transilien-ile-de-france.json",function(result){
+        format: "json",
+        $.each(result, function(i, field){
+            items.push(field.fields.code_insee_commune.substr(0,2));
+        });
+        items.sort();
+        items=items.clearDoublon();
+        for(var i = 0 ; i < items.length ; i++)
+        {
+            $("#menu > #nav > .nav-primary > li").next(".sub-menu").append("<li>"+items[i]+"</li>");
+        }
+    });
     $("#menu > #nav > .nav-primary > li.level0 > a").click(function(ev){
         if($(this).attr("class")=="active"){
             $(this).parents("li").next(".sub-menu").slideUp();
